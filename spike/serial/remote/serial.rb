0c421f7f7e5edfdbc7240e029ba1a01d67aa6359
Coney Wu <coney.wu@gmail.com>
2014-03-05 23:48:58 +0800

    update serial spike

---
+++ spike/serial/remote/serial.rb
@@ -0,0 +1,14 @@
+#!/usr/bin/env ruby
+
+require "serialport"
+require "awesome_print"
+
+SerialPort.open("/dev/ttyUSB0", 9600, 8, 1, SerialPort::NONE) do |serial|
+  loop do
+    ap serial.readline
+    serial.write "hello\r\n"
+    serial.write "hello\r\n"
+    serial.write "hello\r\n"
+    serial.write "hello\r\n"
+  end
+end
--- spike/serial/src/main.cpp
+++ spike/serial/src/main.cpp
@@ -8,12 +8,11 @@ void loop();
 #include "lcd1602.h"
 
 
-#define S1TX    18
-#define S1RX    19
+#define S1TX    14
+#define S1RX    15
 
 LCD1602 lcd;
 SoftwareSerial s1(S1RX, S1TX);
-
 void setup()
 {
     lcd.printAt(0, "ready for serial!");
@@ -23,8 +22,10 @@ void setup()
 
 void loop()
 {
-    if (s1.available()) {
-        String str = s1.readString();
+    String str = s1.readString();
+    if (str.length())
+    {
+        lcd.printAt(0, "data arrive");
         lcd.printAt(1, "%s", str.c_str());
     }
 }
